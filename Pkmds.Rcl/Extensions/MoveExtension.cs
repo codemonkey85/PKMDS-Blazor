d9a4ea6d2f263c4f321620b057493924f31f8f45
Michael Bond <michael@bondcodes.com>
2023-02-18 18:54:53 -0500

    Simplify move validation

--- Pkmds.Rcl/Extensions/MoveExtension.cs
+++ Pkmds.Rcl/Extensions/MoveExtension.cs
@@ -2,5 +2,5 @@
 
 public static class MoveExtension
 {
-    public static bool IsValidMove(this Move move) => move is not Move.None or Move.MAX_COUNT;
+    public static bool IsValidMove(this Move move) => move is > Move.None and < Move.MAX_COUNT;
 }
