a7c1804957efacb56d161d0b30a7633cc72628e0
Michael Bond <michael@bondcodes.com>
2023-03-16 18:24:25 -0400

    Clear selection after changing box from dropdown

--- Pkmds.Rcl/Components/PokemonDetailsComponent.razor.cs
+++ Pkmds.Rcl/Components/PokemonDetailsComponent.razor.cs
@@ -5,11 +5,4 @@ public partial class PokemonDetailsComponent : IDisposable
     protected override void OnInitialized() => AppState.OnAppStateChanged += StateHasChanged;
 
     public void Dispose() => AppState.OnAppStateChanged -= StateHasChanged;
-
-    private void ClearSelection()
-    {
-        AppState.SelectedPokemon = null;
-        AppState.SelectedBoxSlot = null;
-        AppState.Refresh();
-    }
 }
