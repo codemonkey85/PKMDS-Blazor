c76bc12f338dd430bf6a24a512c624ae8248fedd
Michael Bond <michael@bondcodes.com>
2023-03-23 19:16:40 -0400

    Add mobile viewport

--- Pkmds.Rcl/Components/BoxComponent.razor.cs
+++ Pkmds.Rcl/Components/BoxComponent.razor.cs
@@ -7,10 +7,6 @@ public partial class BoxComponent : IDisposable
 
     private BoxEdit? BoxEdit { get; set; }
 
-    private Guid subscriptionId;
-
-    private bool previewRow = true;
-
     protected override void OnParametersSet()
     {
         if (AppState.SaveFile is null)
@@ -25,43 +21,9 @@ public partial class BoxComponent : IDisposable
         BoxEdit.LoadBox(BoxId);
     }
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            var subscriptionResult = await BreakpointListener.Subscribe(
-                HandleBreakpoint,
-                new ResizeOptions
-                {
-                    ReportRate = 250,
-                    NotifyOnBreakpointOnly = false,
-                });
-            subscriptionId = subscriptionResult.SubscriptionId;
-        }
-
-        await base.OnAfterRenderAsync(firstRender);
-    }
-
-    private void HandleBreakpoint(MudBlazor.Breakpoint breakpoint)
-    {
-        if (breakpoint switch
-        {
-            MudBlazor.Breakpoint.Xs or
-            MudBlazor.Breakpoint.Sm => false,
-            _ => true,
-        } != previewRow)
-        {
-            previewRow = !previewRow;
-            StateHasChanged();
-        }
-    }
-
     protected override void OnInitialized() =>
         AppState.OnAppStateChanged += StateHasChanged;
 
-    public void Dispose()
-    {
+    public void Dispose() =>
         AppState.OnAppStateChanged -= StateHasChanged;
-        BreakpointListener.Unsubscribe(subscriptionId);
-    }
 }
--- Pkmds.Rcl/GlobalUsings.cs
+++ Pkmds.Rcl/GlobalUsings.cs
@@ -2,7 +2,6 @@
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Forms;
 global using Microsoft.JSInterop;
-global using MudBlazor.Services;
 global using PKHeX.Core;
 global using Pkmds.Rcl.Components;
 global using Pkmds.Rcl.Extensions;
