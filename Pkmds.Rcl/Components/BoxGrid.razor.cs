3df373b7a6142cbdcfca83bc045308672bfdc435
Michael Bond <michael@bondcodes.com>
2023-03-24 10:27:37 -0400

    Fix loading Gen 1 / Gen 2 saves

--- Pkmds.Rcl/Components/BoxGrid.razor.cs
+++ Pkmds.Rcl/Components/BoxGrid.razor.cs
@@ -4,4 +4,7 @@ public partial class BoxGrid
 {
     [Parameter, EditorRequired]
     public BoxEdit? BoxEdit { get; set; }
+
+    private int columns => AppState?.SaveFile?.BoxSlotCount == 20 ? 4 : 6;
+    private const int rows = 5;
 }
