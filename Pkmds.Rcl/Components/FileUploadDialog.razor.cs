66fcb6254b0723aff3178e26f25fe90ec9ce6c4e
Michael Bond <michael@bondcodes.com>
2023-03-24 09:59:48 -0400

    Accept any file type
    At least until I can figure out how to get file uploads working properly on iOS.

---
+++ Pkmds.Rcl/Components/FileUploadDialog.razor.cs
@@ -0,0 +1,17 @@
+using MudBlazor;
+
+namespace Pkmds.Rcl.Components;
+
+public partial class FileUploadDialog
+{
+    [CascadingParameter]
+    private MudDialogInstance MudDialog { get; set; } = default!;
+
+    private IBrowserFile? browserFile;
+
+    private void HandleFile(IBrowserFile file) => browserFile = file;
+
+    private void Cancel() => MudDialog.Close(DialogResult.Cancel());
+
+    private void Ok() => MudDialog.Close(DialogResult.Ok(browserFile));
+}
