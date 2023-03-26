a5137a24dd65d724e44700cf8860ea6bac0835c9
Michael Bond <michael@bondcodes.com>
2023-03-26 17:26:02 -0400

    Initial support for dark theme

--- Pkmds.Rcl/GlobalUsings.cs
+++ Pkmds.Rcl/GlobalUsings.cs
@@ -2,6 +2,7 @@
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Forms;
 global using Microsoft.JSInterop;
+global using MudBlazor;
 global using PKHeX.Core;
 global using Pkmds.Rcl.Components;
 global using Pkmds.Rcl.Extensions;
--- Pkmds.Rcl/Shared/MainLayout.razor.cs
+++ Pkmds.Rcl/Shared/MainLayout.razor.cs
@@ -3,6 +3,25 @@ namespace Pkmds.Rcl.Shared;
 public partial class MainLayout
 {
     private bool drawerOpen = true;
+    private bool isDarkMode;
+    private MudThemeProvider? mudThemeProvider;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender && mudThemeProvider is not null)
+        {
+            isDarkMode = await mudThemeProvider.GetSystemPreference();
+            await mudThemeProvider.WatchSystemPreference(OnSystemPreferenceChanged);
+            StateHasChanged();
+        }
+    }
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+    private async Task OnSystemPreferenceChanged(bool newValue)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+    {
+        isDarkMode = newValue;
+        StateHasChanged();
+    }
 
     private void DrawerToggle() => drawerOpen = !drawerOpen;
 
