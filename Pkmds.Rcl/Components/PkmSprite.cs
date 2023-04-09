5411c31a253f451ee26b664736235e442fa8d795
Michael Bond <michael@bondcodes.com>
2023-04-09 10:52:12 -0400

    Sprite fixes

---
+++ Pkmds.Rcl/Components/PkmSprite.cs
@@ -0,0 +1,17 @@
+﻿namespace Pkmds.Rcl.Components;
+
+public class PkmSprite : MudImage
+{
+    [Parameter, EditorRequired] public PKM? Pokemon { get; set; }
+
+    public PkmSprite()
+    {
+        ObjectFit = ObjectFit.Contain;
+        ObjectPosition = ObjectPosition.Center;
+    }
+
+    protected override void OnParametersSet()
+    {
+        Src = SpriteHelper.GetPokemonSpriteFilename(Pokemon);
+    }
+}
