8c8f91d1933e7c9920e59399e613fc38c68e905e
Michael Bond <michael@bondcodes.com>
2023-04-10 16:21:31 -0400

    Re-add clear button

--- Pkmds.Rcl/Components/PokemonEditForm.razor.cs
+++ Pkmds.Rcl/Components/PokemonEditForm.razor.cs
@@ -1,5 +1,8 @@
 namespace Pkmds.Rcl.Components;
 
-public partial class PokemonEditForm
+public partial class PokemonEditForm : IDisposable
 {
+    protected override void OnInitialized() => AppState.OnAppStateChanged += StateHasChanged;
+
+    public void Dispose() => AppState.OnAppStateChanged -= StateHasChanged;
 }
