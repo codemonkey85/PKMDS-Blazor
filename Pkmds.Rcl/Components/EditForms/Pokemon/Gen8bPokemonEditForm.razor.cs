bc307acd0ac4f0199d12843b1f7ef77258032832
Michael Bond <michael@bondcodes.com>
2023-04-12 18:24:18 -0400

    Consolidate editor backing code

--- Pkmds.Rcl/Components/EditForms/Pokemon/Gen8bPokemonEditForm.razor.cs
+++
@@ -1,8 +0,0 @@
-namespace Pkmds.Rcl.Components.EditForms.Pokemon;
-
-public partial class Gen8bPokemonEditForm : IDisposable
-{
-    protected override void OnInitialized() => AppState.OnAppStateChanged += StateHasChanged;
-
-    public void Dispose() => AppState.OnAppStateChanged -= StateHasChanged;
-}
