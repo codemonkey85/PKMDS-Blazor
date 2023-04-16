e54d922776a448b825a2bc892064e1b61a66d364
Michael Bond <michael@bondcodes.com>
2023-04-16 17:23:23 -0400

    A whole bunch of cleanup

--- Pkmds.Rcl/wwwroot/js/chart.js
+++
@@ -1,8 +0,0 @@
-﻿var charts = new Object();
-window.setupChart = (id, config) => {
-    var ctx = document.getElementById(id).getContext('2d');
-    if (typeof charts[id] !== 'undefined') {
-        charts[id].destroy();
-    }
-    charts[id] = new Chart(ctx, config);
-}
