bff5d2935a2ee70852f571826a009a198d3b3f86
Michael Bond <michael@bondcodes.com>
2023-04-17 03:24:34 -0400

    Fix grid layout for games with 20 box slots

--- Pkmds.Rcl/Components/BoxGrid.razor.cs
+++ Pkmds.Rcl/Components/BoxGrid.razor.cs
@@ -5,9 +5,9 @@ public partial class BoxGrid
     [Parameter, EditorRequired]
     public BoxEdit? BoxEdit { get; set; }
 
-    private int GridHeight => (56 + 4) * (AppState?.SaveFile?.BoxSlotCount == 20 ? 4 : 6);
+    private static int GridHeight => (56 + 4) * 6;
 
-    private static int GridWidth => (68 + 4) * 6;
+    private int GridWidth => (68 + 4) * (AppState.SaveFile?.BoxSlotCount == 20 ? 4 : 6);
 
     private string GridStyle => $"width: {GridWidth}px; height: {GridHeight}px;";
 }
