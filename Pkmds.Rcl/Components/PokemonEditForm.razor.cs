a10e426b865ece9a6d850bb161d959daef8a4ff4
Michael Bond <michael@bondcodes.com>
2023-04-16 23:42:40 -0400

    Drop the idea of separate PKM editor components

--- Pkmds.Rcl/Components/PokemonEditForm.razor.cs
+++
@@ -1,8 +0,0 @@
-namespace Pkmds.Rcl.Components;
-
-public partial class PokemonEditForm : IDisposable
-{
-    protected override void OnInitialized() => AppState.OnAppStateChanged += StateHasChanged;
-
-    public void Dispose() => AppState.OnAppStateChanged -= StateHasChanged;
-}
