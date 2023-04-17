d34ac434f0a44d7e4cb98b2d01229567917028e4
Michael Bond <michael@bondcodes.com>
2023-04-17 02:30:51 -0400

    Fix forms (hopefully)

--- Pkmds.Rcl/Components/EditForms/PokemonEditForm.razor.cs
+++ Pkmds.Rcl/Components/EditForms/PokemonEditForm.razor.cs
@@ -2,4 +2,17 @@ namespace Pkmds.Rcl.Components.EditForms;
 
 public partial class PokemonEditForm
 {
+    private MudSelect<byte>? FormSelect { get; set; }
+
+    protected override void OnInitialized() =>
+        AppState.OnAppStateChanged += Refresh;
+
+    public void Dispose() =>
+        AppState.OnAppStateChanged -= Refresh;
+
+    private void Refresh()
+    {
+        FormSelect?.ForceRender(true);
+        StateHasChanged();
+    }
 }
