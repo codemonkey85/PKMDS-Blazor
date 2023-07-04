50058c3c3f4985d0825a6f18e110dd0b0dd70ec8
Michael Bond <michael@bondcodes.com>
2023-07-03 21:00:53 -0400

    Add party grid, still needs visual tweaks

---
+++ Pkmds.Rcl/Components/PartyGrid.razor.cs
@@ -0,0 +1,16 @@
+namespace Pkmds.Rcl.Components;
+
+public partial class PartyGrid
+{
+    private static int GridHeight => (56 + 4) * 3;
+
+    private int GridWidth => (68 + 4) * 2;
+
+    private string GridStyle => $"width: {GridWidth}px; height: {GridHeight}px;";
+
+    protected override void OnInitialized() =>
+        AppState.OnAppStateChanged += StateHasChanged;
+
+    public void Dispose() =>
+        AppState.OnAppStateChanged -= StateHasChanged;
+}
