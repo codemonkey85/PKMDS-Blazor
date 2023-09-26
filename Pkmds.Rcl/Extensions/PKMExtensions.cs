e4d10a802ad44fa827a189d4afb54e8af798b269
Michael Bond <michael@bondcodes.com>
2023-09-26 14:10:38 -0400

    Fix types, conditionally hide stuff in main tab

--- Pkmds.Rcl/Extensions/PKMExtensions.cs
+++ Pkmds.Rcl/Extensions/PKMExtensions.cs
@@ -4,4 +4,17 @@ public static class PKMExtensions
 {
     public static uint? GetFormArgument(this PKM pkm, uint? valueIfNull = null) =>
         (pkm as IFormArgument)?.FormArgument ?? valueIfNull;
+
+    public static (byte Type1, byte Type2) GetGenerationTypes(this PKM pkm)
+    {
+        var type1 = pkm.PersonalInfo.Type1;
+        var type2 = pkm.PersonalInfo.Type2;
+        var generation = pkm.Generation;
+
+        return generation <= 2
+            ? (ConvertGenerationType(type1, generation), ConvertGenerationType(type2, generation))
+            : (type1, type2);
+
+        static byte ConvertGenerationType(byte type, int generation) => (byte)((MoveType)type).GetMoveTypeGeneration(generation);
+    }
 }
