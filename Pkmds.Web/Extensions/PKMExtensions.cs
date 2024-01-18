3c8bf624d79f503944697531a793f055aa999688
Michael Bond <michael@bondcodes.com>
2024-01-18 10:22:35 -0500

    Upgrade package, reimplement markings

--- Pkmds.Web/Extensions/PKMExtensions.cs
+++ Pkmds.Web/Extensions/PKMExtensions.cs
@@ -17,4 +17,25 @@ public static class PKMExtensions
 
         static byte ConvertGenerationType(byte type, int generation) => (byte)((MoveType)type).GetMoveTypeGeneration(generation);
     }
+
+    public static int GetMarking(this PKM pokemon, int index)
+    {
+        if (pokemon is not IAppliedMarkings appliedMarkings)
+        {
+            throw new ArgumentException("Pokemon does not implement IAppliedMarkings", nameof(pokemon));
+        }
+
+        if ((uint)index >= appliedMarkings.MarkingCount)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        return pokemon switch
+        {
+            IAppliedMarkings7 markings7 => (int)markings7.GetMarking(index),
+            IAppliedMarkings4 markings4 => markings4.GetMarking(index) ? 0 : 1,
+            IAppliedMarkings3 markings3 => markings3.GetMarking(index) ? 0 : 1,
+            _ => throw new ArgumentException("Pokemon does not implement IAppliedMarkings", nameof(pokemon)),
+        };
+    }
 }
