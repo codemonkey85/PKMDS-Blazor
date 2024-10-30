2aa2f980965944ebca9049d8cee27656081e4028
Michael Bond <michael@bondcodes.com>
2024-10-30 18:02:46 -0400

    Fix page title rendering

--- Pkmds.Web/Components/SaveFileComponent.razor.cs
+++ Pkmds.Web/Components/SaveFileComponent.razor.cs
@@ -5,4 +5,32 @@ public partial class SaveFileComponent : IDisposable
     protected override void OnInitialized() => RefreshService.OnAppStateChanged += StateHasChanged;
 
     public void Dispose() => RefreshService.OnAppStateChanged -= StateHasChanged;
+
+    private string SaveFileNameDisplayString()
+    {
+        const string baseTitle = "PKMDS Save Editor";
+
+        if (AppState.SaveFile is not { } saveFile)
+        {
+            return baseTitle;
+        }
+
+        var sbTitle = new StringBuilder(baseTitle).Append(" - ");
+
+        sbTitle.Append($"{saveFile.OT} ");
+        
+        if (saveFile.Context is not EntityContext.Gen1)
+        {
+            var genderDisplay = saveFile.Gender == (byte)Gender.Male ? Constants.MaleGenderUnicode : Constants.FemaleGenderUnicode;
+            sbTitle.Append($"{genderDisplay} ");
+        }
+        
+        sbTitle.Append($"({saveFile.DisplayTID.ToString(AppService.GetIdFormatString())}, ");
+        
+        sbTitle.Append($"{SaveFileNameDisplay.FriendlyGameName(saveFile.Version)}, ");
+        
+        sbTitle.Append($"{saveFile.PlayTimeString})");
+
+        return sbTitle.ToString();
+    }
 }
