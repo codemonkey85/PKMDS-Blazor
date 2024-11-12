1cf50e5796ee5fcf8824efd08900963c52becca3
codemonkey85 <codemonkey85@users.noreply.github.com>
2024-11-12 20:48:40 +0000

    Deploying to gh-pages from @ codemonkey85/PKMDS-Blazor@b3f771967d41a80f6f1bec4b840b6e7145ace019 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+/* Manifest version: z2JlpOnP */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
@@ -53,4 +54,3 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: N3gbrjQf */
