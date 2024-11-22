355a87e264126cba8f2fb741f11be044347a6842
codemonkey85 <michael@bondcodes.com>
2024-11-22 04:15:46 +0000

    Deploying to gh-pages from @ codemonkey85/PKMDS-Blazor@d4496039ae2977102b828fce73a85f9d063d089b 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: dYM42BAu */
+/* Manifest version: tpOLTLr7 */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
