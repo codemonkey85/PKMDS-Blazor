0fe4512f082db5972a14f9d6a117c81c8a6ff3cb
Michael Bond <michael@bondcodes.com>
2024-12-17 17:19:15 -0500

    Code adjustments

--- Pkmds.Web/Extensions/PkmExtensions.cs
+++ Pkmds.Web/Extensions/PkmExtensions.cs
@@ -15,7 +15,8 @@ public static class PkmExtensions
             ? (ConvertGenerationType(type1, generation), ConvertGenerationType(type2, generation))
             : (type1, type2);
 
-        static byte ConvertGenerationType(byte type, byte generation) => (byte)((MoveType)type).GetMoveTypeGeneration(generation);
+        static byte ConvertGenerationType(byte type, byte generation) =>
+            (byte)((MoveType)type).GetMoveTypeGeneration(generation);
     }
 
     public static int GetMarking(this PKM pokemon, int index)
@@ -40,26 +41,26 @@ public static class PkmExtensions
 
     // ReSharper disable once InconsistentNaming
     public static ReadOnlyCollection<int> GetPP(this PKM pokemon) => new(
-        [
-            pokemon.Move1_PP,
-            pokemon.Move2_PP,
-            pokemon.Move3_PP,
-            pokemon.Move4_PP
-        ]);
+    [
+        pokemon.Move1_PP,
+        pokemon.Move2_PP,
+        pokemon.Move3_PP,
+        pokemon.Move4_PP
+    ]);
 
     // ReSharper disable once InconsistentNaming
     public static ReadOnlyCollection<int> GetPPUps(this PKM pokemon) => new(
-        [
-            pokemon.Move1_PPUps,
-            pokemon.Move2_PPUps,
-            pokemon.Move3_PPUps,
-            pokemon.Move4_PPUps
-        ]);
+    [
+        pokemon.Move1_PPUps,
+        pokemon.Move2_PPUps,
+        pokemon.Move3_PPUps,
+        pokemon.Move4_PPUps
+    ]);
 
     // ReSharper disable once InconsistentNaming
     public static void SetPP(this PKM pokemon, int moveIndex, int pp)
     {
-        if(pp < 0)
+        if (pp < 0)
         {
             pp = 0;
         }
@@ -107,7 +108,7 @@ public static class PkmExtensions
     }
 
     // ReSharper disable once InconsistentNaming
-    public static int GetMaxPP(this PKM pokemon, int moveIndex) 
+    public static int GetMaxPP(this PKM pokemon, int moveIndex)
     {
         var move = pokemon.GetMove(moveIndex);
         // ReSharper disable once InconsistentNaming
