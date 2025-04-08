dc7eadb04047a6adfe12738b37fd2b788c057c52
Michael Bond <michael@bondcodes.com>
2025-04-08 17:52:03 -0400

    A whole bunch of cleanup

--- Pkmds.Maui/Platforms/iOS/AppDelegate.cs
+++ Pkmds.Maui/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace Pkmds.Maui;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
