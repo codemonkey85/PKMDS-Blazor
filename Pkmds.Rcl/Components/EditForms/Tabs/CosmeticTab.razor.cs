0d60ff26947cc1c942af171ebafcda99fde5b038
Michael Bond <michael@bondcodes.com>
2025-11-07 00:31:16 -0500

    Add scale controls for IScaledSize and IScaledSize3 Pokémon
    Added numeric fields to CosmeticTab for editing HeightScalar and WeightScalar when the Pokémon implements IScaledSize, and a scale field with rating adornment for IScaledSize3. Also added helper methods to display scale ratings appropriately.

--- Pkmds.Rcl/Components/EditForms/Tabs/CosmeticTab.razor.cs
+++ Pkmds.Rcl/Components/EditForms/Tabs/CosmeticTab.razor.cs
@@ -2,7 +2,8 @@ namespace Pkmds.Rcl.Components.EditForms.Tabs;
 
 public partial class CosmeticTab : IDisposable
 {
-    [Parameter, EditorRequired]
+    [Parameter]
+    [EditorRequired]
     public PKM? Pokemon { get; set; }
 
     public void Dispose() =>
@@ -10,4 +11,18 @@ public partial class CosmeticTab : IDisposable
 
     protected override void OnInitialized() =>
         RefreshService.OnAppStateChanged += StateHasChanged;
+
+    private string GetScaleRating(IScaledSize3 scaledSize3) => Pokemon switch
+    {
+        PK9 => GetString(PokeSizeDetailedUtil.GetSizeRating(scaledSize3.Scale)),
+        _ => GetString(PokeSizeUtil.GetSizeRating(scaledSize3.Scale))
+    };
+
+    private static string GetString(PokeSize size) => size is PokeSize.AV
+        ? "M"
+        : size.ToString();
+
+    private static string GetString(PokeSizeDetailed size) => size is PokeSizeDetailed.AV
+        ? "M"
+        : size.ToString();
 }
