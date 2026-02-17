58b974027b98e205c31541b14f5ce3b9e7c24a89
Michael Bond <michael@bondcodes.com>
2026-02-16 21:58:58 -0500

    Remove unused global using directives
    Clean up GlobalUsings.cs across projects by removing unused/global imports. Changes remove several redundant usings from Pkmds.Core (System.Collections.Generic, System.ComponentModel, System.Diagnostics.CodeAnalysis, System.Globalization, System.Text), Pkmds.Rcl (System.Collections.ObjectModel, Pkmds.Rcl.Extensions) and Pkmds.Tests (Pkmds.Core.Utilities, Pkmds.Rcl.Extensions). This is a non-functional cleanup to reduce noise and unused dependencies.

--- Pkmds.Core/GlobalUsings.cs
+++ Pkmds.Core/GlobalUsings.cs
@@ -1,7 +1,2 @@
-global using System.Collections.Generic;
 global using System.Collections.ObjectModel;
-global using System.ComponentModel;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Globalization;
-global using System.Text;
 global using PKHeX.Core;
